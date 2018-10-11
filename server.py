8e36ecba571d30c5bdf0efee4469bc3cbc6fbce2
David Ben Knoble <ben.knoble@gmail.com>
2018-10-11 14:18:44 -0400

    Integrate flask and gunicorn logging
    cf. https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f

--- server.py
+++ server.py
@@ -56,3 +56,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
